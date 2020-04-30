bc1ef3cc6b6fdd6e67ae0e18d816378bfac9cad9
Christopher Smith <chshsmit@gmail.com>
2020-04-29 17:33:58 -0700

    Started to add authentication to the backend

--- weboredgames-server/src/index.js
+++ weboredgames-server/src/index.js
@@ -3,17 +3,22 @@
 * @author Christopher Smith
 * @description
 * @created 2020-04-11T10:50:30.991Z-07:00
-* @last-modified 2020-04-26T15:23:36.988Z-07:00
+* @last-modified 2020-04-29T17:26:15.579Z-07:00
 */
 
 // ----------------------------------------------------
 
 const express = require('express');
+const session = require('express-session');
 const socketio = require('socket.io');
 const http = require('http');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const MongoStore = require('connect-mongo')(session);
 
+const passport = require('./passport/setup');
+
+const auth = require('./routes/auth');
 const health = require('./routes/health');
 
 const roomManagement = require('./sockets/Room/roomManagement');
@@ -26,12 +31,47 @@ const PORT = process.env.PORT || 5000;
 
 // ----------------------------------------------------
 
-mongoose.connect(process.env.MONGO_DB_CONNECTION, { useNewUrlParser: true });
+mongoose.connect(process.env.MONGO_DB_CONNECTION, { useNewUrlParser: true }, () => {
+  console.log("Connected to mongoDB");
+});
 
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
+// ----------------------------------------------------
+// Middlewares
+// ----------------------------------------------------
+
+// Passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Body parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Express Session
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET_KEY,
+    resave: false,
+    saveUninitialized: true,
+    store: new MongoStore({ mongooseConnection: mongoose.connection })
+  })
+);
+
+
+// Routes
+app.use("/api/auth", auth);
+app.use(health);
+
+// Cors
+app.use(cors);
+
+
+// ----------------------------------------------------
+// Socket management
 // ----------------------------------------------------
 
 io.on('connection', (socket) => {
@@ -46,7 +86,10 @@ io.on('connection', (socket) => {
 });
 
 
-app.use(health);
-app.use(cors);
+// ----------------------------------------------------
+// Socket management
+// ----------------------------------------------------
+
+
 
 server.listen(PORT, () => console.log(`Server has started on port ${PORT}`));
---
+++ weboredgames-server/src/passport/setup.js
@@ -0,0 +1,79 @@
+/**
+* setup.js
+* @author Christopher Smith
+* @description
+* @created 2020-04-29T16:43:19.879Z-07:00
+* @copyright
+* @last-modified 2020-04-29T17:29:59.557Z-07:00
+*/
+
+// ----------------------------------------------------
+
+const bcrypt = require('bcrypt');
+const User = require('../models/User/User');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+
+// ----------------------------------------------------
+
+passport.serializeUser((user, done) => {
+  console.log(user);
+  done(null, user.id);
+});
+
+// ----------------------------------------------------
+
+passport.deserializeUser((id, done) => {
+  console.log(id)
+  User.findById(id, (err, user) => {
+    done(err, user);
+  });
+});
+
+// ----------------------------------------------------
+
+// Local Strategy
+passport.use(
+  new LocalStrategy({ usernameField: "email" }, (email, password, done) => {
+    console.log("Are we ever here")
+    // Match the user
+    User.findOne({ _email: email })
+      .then(user => {
+        //Create new user
+        if (!user) {
+          const newUser = new User({ _email: email, _password: password });
+          bcrypt.genSalt(10, (err, salt) => {
+            bcrypt.hash(newUser._password, salt, (err, hash) => {
+              if (err) throw err;
+              newUser._password = hash;
+              newUser.save()
+                .then(user => {
+                  // console.log(user);
+                  return done(null, user);
+                })
+                .catch(err => {
+                  console.log("Is there an error")
+                  console.log(err);
+                  return done(null, false, { message: err });
+                });
+            });
+          });
+        } else {
+          // Match Password
+          bcrypt.compare(password, user._password, (err, isMatch) => {
+            if (err) throw err;
+            if (isMatch) {
+              return done(null, user);
+            } else {
+              return done(null, false, { message: "Wrong Password" });
+            }
+          });
+        }
+      })
+      .catch(err => {
+        return done(null, false, { message: err });
+      });
+  })
+);
+
+module.exports = passport;
