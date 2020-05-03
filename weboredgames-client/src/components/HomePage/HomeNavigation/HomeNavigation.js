2c1d8e69e519858f6b6a4c093e85b8eb57dd2c0a
Christopher Smith <chshsmit@gmail.com>
2020-05-02 22:57:11 -0700

    Need to figure out this whole navbar thing

--- weboredgames-client/src/components/HomePage/HomeNavigation/HomeNavigation.js
+++ weboredgames-client/src/components/HomePage/HomeNavigation/HomeNavigation.js
@@ -3,7 +3,7 @@
 * @author Christopher Smith
 * @description The main navbar component
 * @created 2020-05-02T22:19:20.242Z-07:00
-* @last-modified 2020-05-02T22:47:03.314Z-07:00
+* @last-modified 2020-05-02T22:56:53.556Z-07:00
 */
 
 // -----------------------------------------------------------
@@ -15,7 +15,15 @@ import PropTypes from 'prop-types';
 import {
   Navbar,
   NavbarBrand,
-  UncontrolledDropdown
+  UncontrolledDropdown,
+  Nav,
+  NavItem,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  Collapse,
+  NavbarToggler,
+  Dropdown
 } from 'reactstrap';
 
 import './HomeNavigation.css';
@@ -28,7 +36,28 @@ const HomeNavigation = ({ auth }) => {
 
   return (
     <Navbar color="light" light id="mainNavbar">
-      <NavbarBrand>WeBoredGames</NavbarBrand>
+      <NavbarBrand href="/home">WeBoredGames</NavbarBrand>
+      <Collapse isOpen>
+        <Nav navbar>
+          <Dropdown nav inNavbar>
+            <DropdownToggle nav caret>
+              Options
+            </DropdownToggle>
+            <DropdownMenu right>
+              <DropdownItem>
+                Option 1
+              </DropdownItem>
+              <DropdownItem>
+                Option 2
+              </DropdownItem>
+              <DropdownItem divider />
+              <DropdownItem>
+                Reset
+              </DropdownItem>
+            </DropdownMenu>
+          </Dropdown>
+        </Nav>
+      </Collapse>
     </Navbar>
   );
 
