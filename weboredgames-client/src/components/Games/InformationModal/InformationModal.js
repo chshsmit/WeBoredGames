6ad3629cce1e3ad752762ff572e7915d5805a19f
Christopher Smith <chshsmit@gmail.com>
2020-05-06 16:12:57 -0700

    Fixed problem with modal size

--- weboredgames-client/src/components/Games/InformationModal/InformationModal.js
+++ weboredgames-client/src/components/Games/InformationModal/InformationModal.js
@@ -3,7 +3,7 @@
 * @author Christopher Smith
 * @description Information modal for all of the games
 * @created 2020-05-05T13:42:45.276Z-07:00
-* @last-modified 2020-05-05T18:12:36.769Z-07:00
+* @last-modified 2020-05-06T16:12:37.534Z-07:00
 */
 
 // ----------------------------------------------------
@@ -27,6 +27,7 @@ const InformationModal = ({ gameName, isOpen, toggle }) => {
       size="lg"
       isOpen={isOpen}
       toggle={toggle}
+      className="information-modal"
     >
       <ModalHeader toggle={toggle}>{gameName} Information</ModalHeader>
       {determineModalComponent(gameName)}
